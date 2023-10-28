c64d9b67dc67aa6f2c53d7892596046604753dd7
meneerjelle <jelle@willems.email>
2023-10-29 00:42:00 +0200

    finished

--- script.js
+++ script.js
@@ -1,17 +1,86 @@
 const RockPaperScissors = ["steen", "papier", "schaar"];
-const random = Math.floor(Math.random() * RockPaperScissors.length);
 
-let computerSelection = RockPaperScissors[random];
+const choiceRock = document.querySelector("#steen");
+const choicePaper = document.querySelector("#papier");
+const choiceScissors = document.querySelector("#schaar");
 
-let playerSelection = prompt("Choose Rock, Paper or Scissors.").toLowerCase();
+const result = document.querySelector("#result");
+const scoreOutput = document.querySelector("#scoreOutput");
 
-if (playerSelection === computerSelection) {
-    alert("Tie!");
-} else if ((playerSelection === "steen" && computerSelection === "schaar") || 
-          (playerSelection === "papier" && computerSelection === "steen") ||
-          (playerSelection === "schaar" && computerSelection === "papier"))
-{
-    alert("Win!");
-} else {
-    alert("Lose!");
+const restartGame = document.querySelector("#restart");
+
+let userScore = 0;
+let computerScore = 0;
+
+scoreOutput.innerHTML = `Jij: <strong>${userScore}</strong> Computer: <strong>${computerScore}</strong>`;
+
+choiceRock.addEventListener("click", function(event) { 
+    event.preventDefault()
+    runGame("steen");
+});
+choicePaper.addEventListener("click", function(event) {
+    event.preventDefault()
+    runGame("papier");
+});
+choiceScissors.addEventListener("click", function(event) {
+    event.preventDefault()
+    runGame("schaar");
+});
+
+restartGame.addEventListener("click", function() {
+    userScore = 0;
+    computerScore = 0;
+
+    result.innerHTML = "";
+    scoreOutput.innerHTML = `Jij: <strong>${userScore}</strong> Computer: <strong>${computerScore}</strong>`;
+
+
+})
+
+
+
+function runGame(playerSelection) {
+    
+    const random = Math.floor(Math.random() * RockPaperScissors.length);
+    const computerSelection = RockPaperScissors[random];
+
+    if (userScore === 3 || computerScore === 3) {
+        return;
+    }
+
+        if (playerSelection === "steen" && computerSelection === "schaar" ||
+            playerSelection === "papier" && computerSelection === "steen" ||
+            playerSelection === "schaar" && computerSelection === "papier"
+        ) {
+            result.innerHTML = `Computer koos <strong>${computerSelection}</strong>. <span style="color: green;">Punt voor jou!</span>`;
+            userScore += 1;
+            
+        } else if (playerSelection.value === computerSelection) {
+            result.innerHTML = `Jullie kozen allebei <strong>${computerSelection}</strong>. Gelijk!`;
+    
+        } else {
+            result.innerHTML = `Computer koos <strong>${computerSelection}</strong>. <span style="color: red;">Punt voor computer!</span>`;
+            computerScore += 1;
+    
+        } 
+        
+        scoreOutput.innerHTML = `Jij: <strong>${userScore}</strong> Computer: <strong>${computerScore}</strong>`;
+    
+        if (userScore === 3) {
+            scoreOutput.innerHTML = `<span style="font-size: 3rem; color: green;">GEWONNEN!</span>`;
+            disableGame();
+
+        } else if (computerScore === 3) {
+            scoreOutput.innerHTML = `<span style="font-size: 3rem; color: red;">VERLOREN!</span>`;
+            disableGame();
+        }
+    
+        }
+
+function disableGame() {
+    choiceRock.disabled = true;
+    choicePaper.disabled = true;
+    choiceScissors.disabled = true;
+
 }
+
